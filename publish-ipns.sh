a208d93a20079748c6dd625a43ec41cc297d4e66
zekaf <jose.faisca@gmail.com>
2016-09-11 01:26:03 +0000

    Update

---
+++ publish-ipns.sh
@@ -0,0 +1,48 @@
+
+#!/bin/bash
+#
+# Publish to a permanent uri using IPFS/IPNS
+# uri = /ipns/<PEER_ID>/<FILE_NAME>
+#
+# Step 1 - Create a publish directory
+# > mkdir -p /opt/publish
+#
+# Step 2 - Copy files to the publish directory
+# > cp index.html /opt/publish
+#
+# Step 3 - Run this script:
+# ./publish-ipns.sh /opt/publish/
+#
+# Whenever you update your files, just do step 3
+# again, and IPNS will make sure anyone asking for
+# your peerID gets the hash of your latest files.
+#
+
+
+# validate args
+if [ $# -ne 1 ] ; then
+   echo ""
+   echo "Usage: ${0} '<directory>"
+   echo "<directory> directory to publish"
+   echo ""
+   exit 1
+fi
+
+# generate uri
+DIR="$1"
+OUTPUT1=$(ipfs add $FILE --recursive | tail -1) # file hash
+DIR_HASH=$(echo $OUTPUT1 | awk '{print $2}' | xargs)
+OUTPUT2=$(ipfs name publish $DIR_HASH)
+OUTPUT3=$(echo $OUTPUT2 | awk '{print $3}' | xargs)
+PEER_ID=${OUTPUT3: : -1} # Peer ID
+OUTPUT4=$(ipfs name resolve $PEER_ID)
+
+# check
+if [ "$OUTPUT4" == "/ipfs/$DIR_HASH" ] ; then
+	URI="/ipns/$PEER_ID/" # URI
+	echo $URI
+	exit 0
+   else
+	echo "ERROR ..."
+	exit 1
+fi
--- run-ipfs-docker.sh
+++ run-ipfs-docker.sh
@@ -10,20 +10,25 @@ if [ -z "$1" ]; then
  exit 1
 fi
 
-STAGING_DIR="/opt/docker/data/ipfs-export"
-DATA_DIR="/opt/docker/data/ipfs"
+STAGING_DIR="/opt/docker/export"
+DATA_DIR="/opt/docker/data"
 
 # Docker image
 IMG="zekaf/go-ipfs"
 TAG="latest"
 
+# Docker network
+NET_NAME="isolated_nw"
+IPFS_IP="10.17.0.6"
+
 # Docker IPFS container
 IPFS_CT="$1"
 
 docker run -d \
+	--net $NET_NAME --ip $IPFS_IP \
 	--name $IPFS_CT \
 	--restart=always \
-	--volume $STAGING_DIR:/export --volume $DATA_DIR:/data/ipfs \
+	--volume $STAGING_DIR/$IPFS_CT:/export --volume $DATA_DIR/$IPFS_CT:/data/ipfs \
 	-p 4001:4001 -p 5001:5001 -p 8080:8080 \
 	$IMG:$TAG
 
